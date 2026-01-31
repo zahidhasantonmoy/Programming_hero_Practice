26d7579ceb542b5c2fd5c60ea63e00960f9ef023
zahidhasantonmoy <zahidhasantonmoy360@gmail.com>
2026-01-31 21:46:03 +0600

    Add image assets and create a test HTML layout
    - Added various image assets including social media icons and background images.
    - Created a test HTML file with a structured layout using CSS for responsive design.
    - Implemented a background container with centered images for visual presentation.

---
+++ module 14.5/array.js
@@ -0,0 +1,6 @@
+var fruits = ['apple', 'banana', 'cherry', 'date'];
+
+// Accessing elements
+console.log(fruits[3]); // Output: apple
+fruits[2] = 'jambura';
+console.log(fruits); // Output: ['apple', 'banana', 'jambura', 'date']
